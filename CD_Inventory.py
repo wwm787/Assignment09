18d5e8eb3c1192709df4c42210088b2fd3d3c448
wwm787 <100507285+wwm787@users.noreply.github.com>
2022-03-28 21:10:49 -0700

    Add files via upload

---
+++ CD_Inventory.py
@@ -0,0 +1,74 @@
+#------------------------------------------#
+# Title: CD_Inventory.py
+# Desc: The CD Inventory App main Module
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+# DBiesinger, 2030-Jan-02, Extended functionality to add tracks
+# Bill McGinty, 2022-Mar-28, Added functionality for LAB09A (Assignment09)
+#------------------------------------------#
+
+import ProcessingClasses as PC
+import IOClasses as IO
+
+lstFileNames = ['AlbumInventory.txt', 'TrackInventory.txt']
+lstOfCDObjects = IO.FileIO.load_inventory(lstFileNames)
+
+while True:
+    IO.ScreenIO.print_menu()
+    strChoice = IO.ScreenIO.menu_choice()
+
+    if strChoice == 'x':
+        break
+    if strChoice == 'l':
+        print('WARNING: If you continue, all unsaved data will be lost and the Inventory re-loaded from file.')
+        strYesNo = input(
+            'type \'yes\' to continue and reload from file. otherwise reload will be canceled')
+        if strYesNo.lower() == 'yes':
+            print('reloading...')
+            lstOfCDObjects = IO.FileIO.load_inventory(lstFileNames)
+            IO.ScreenIO.show_inventory(lstOfCDObjects)
+        else:
+            input(
+                'canceling... Inventory data NOT reloaded. Press [ENTER] to continue to the menu.')
+            IO.ScreenIO.show_inventory(lstOfCDObjects)
+        continue  # start loop back at top.
+    elif strChoice == 'a':
+        tplCdInfo = IO.ScreenIO.get_CD_info()
+        PC.DataProcessor.add_CD(tplCdInfo, lstOfCDObjects)
+        IO.ScreenIO.show_inventory(lstOfCDObjects)
+        continue  # start loop back at top.
+    elif strChoice == 'd':
+        IO.ScreenIO.show_inventory(lstOfCDObjects)
+        continue  # start loop back at top.
+    elif strChoice == 'c':
+        IO.ScreenIO.show_inventory(lstOfCDObjects)
+        cd_idx = input('Select the CD / Album index: ')
+        cd = PC.DataProcessor.select_cd(lstOfCDObjects, cd_idx)
+        # Copied from DBiesinger LAB09B CDInventory.py, WWM cast to int
+        while True:
+            IO.ScreenIO.print_CD_menu()
+            strChoice = IO.ScreenIO.menu_CD_choice()
+            if strChoice == 'x':
+                break
+            if strChoice == 'a':
+                tplTrkInfo = IO.ScreenIO.get_track_info()
+                PC.DataProcessor.add_track(tplTrkInfo, cd)
+            elif strChoice == 'd':
+                IO.ScreenIO.show_tracks(cd)
+            elif strChoice == 'r':
+                IO.ScreenIO.show_tracks(cd)
+                trk_idx = input('Select the Track index: ')
+                cd.rmv_track(int(trk_idx))
+            else:
+                print('General Error')
+    elif strChoice == 's':
+        IO.ScreenIO.show_inventory(lstOfCDObjects)
+        strYesNo = input('Save this inventory to file? [y/n] ').strip().lower()
+        if strYesNo == 'y':
+            IO.FileIO.save_inventory(lstFileNames, lstOfCDObjects)
+        else:
+            input(
+                'The inventory was NOT saved to file. Press [ENTER] to return to the menu.')
+        continue  # start loop back at top.
+    else:
+        print('General Error')
---
+++ ProcessingClasses.py
@@ -0,0 +1,87 @@
+#------------------------------------------#
+# Title: Processing Classes
+# Desc: A Module for processing Classes
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+# DBiesinger, 2030-Jan-02, Extended functionality to add tracks
+# Bill McGinty, 2022-Mar-28, Added functionality for LAB09A (Assignment09)
+#------------------------------------------#
+
+if __name__ == '__main__':
+    raise Exception('This file is not meant to ran by itself')
+
+import DataClasses as DC
+
+
+class DataProcessor:
+    """Processing the data in the application"""
+    @staticmethod
+    def add_CD(CDInfo, table):
+        """function to add CD info in CDinfo to the inventory table.
+
+
+        Args:
+            CDInfo (tuple): Holds information (ID, CD Title, CD Artist) to be added to inventory.
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime.
+
+        Returns:
+            None.
+
+        """
+
+        cdId, title, artist = CDInfo
+        try:
+            cdId = int(cdId)
+        except:
+            raise Exception('ID must be an Integer!')
+        row = DC.CD(cdId, title, artist)
+        table.append(row)
+
+    @staticmethod
+    def select_cd(table: list, cd_idx: int) -> DC.CD:
+        """selects a CD object out of table that has the ID cd_idx
+
+        Args:
+            table (list): Inventory list of CD objects.
+            cd_idx (int): id of CD object to return
+
+        Raises:
+            Exception: If id is not in list.
+
+        Returns:
+            row (DC.CD): CD object that matches cd_idx
+
+        """
+        incrCD_ID = 0
+        cd_idx = int(cd_idx)
+        while incrCD_ID < len(table):
+            if table[incrCD_ID].cd_id == cd_idx:
+                row = table[incrCD_ID]
+                return row
+            incrCD_ID += 1
+
+    @staticmethod
+    def add_track(track_info: tuple, cd: DC.CD) -> None:
+        """adds a Track object with attributes in track_info to cd
+
+
+        Args:
+            track_info (tuple): Tuple containing track info (position, title, Length).
+            cd (DC.CD): cd object the tarck gets added to.
+
+        Raises:
+            Exception: DESCraised in case position is not an integer.
+
+        Returns:
+            None: DESCRIPTION.
+
+        """
+
+        myTrkList = list(track_info)
+        try:
+            trackPosit = int(myTrkList[0])
+        except:
+            raise Exception('Position must be an Integer')
+        # track is <class 'DataClasses.Track'>
+        track = DC.Track(trackPosit, myTrkList[1], myTrkList[2])
+        cd.add_track(track)
